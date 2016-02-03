c884697fb24ada9c918f27ae62b0ba8b9583e2d4
Emily Horsman <me@emilyhorsman.com>
2016-02-02 21:10:44 -0500

    Replace Promise handling in actions with middleware
    This adds a fetchJSONMiddleware method that is applied on the store. If
    an action is dispatched with a `promise` property it will be handled
    properly given the JSON API. This allows the actions to be cleaned up
    considerably as the repetitive logic for handling Promise resolution is
    no longer duplicated in each action.

--- app/actions/index.jsx
+++ app/actions/index.jsx
@@ -1,74 +1,37 @@
-const fetchOpts = {
-  'credentials': 'include',
-  'mode': 'cors'
-}
-
 export function api(endpoint) {
   return '//api.nightshades.xyz:5000/v1' + endpoint
 }
 
-export function logout(dispatch) {
-  dispatch({ type: 'FETCHING_LOGOUT' })
+const fetchAPI = (endpoint, opts) => {
+  return fetch(api(endpoint), {
+    credentials: 'include',
+    mode: 'cors',
+    ...opts
+  })
+}
 
-  return fetch(api('/logout'), fetchOpts)
-    .then(response => response.json())
-    .then(json => {
-      dispatch({ type: 'FETCHING_LOGOUT_SUCCESS' })
-    })
+export function logout(dispatch) {
+  dispatch({
+    type: 'FETCHING_LOGOUT',
+    promise: fetchAPI('/logout')
+  })
 }
 
 export function fetchMe(dispatch) {
-  dispatch({ type: 'FETCHING_ME' })
-
-  return fetch(api('/me'), fetchOpts)
-    .then(response => response.json())
-    .then(json => {
-      if (json.hasOwnProperty('errors')) {
-        return Promise.reject(json.errors[0].title)
-      }
-
-      dispatch({
-        type: 'FETCHING_ME_SUCCESS',
-        data: json.data,
-        receivedAt: Date.now()
-      })
-    })
-    .catch(e => {
-      dispatch({
-        type: 'FETCHING_ME_ERROR',
-        message: e,
-        receivedAt: Date.now()
-      })
-    })
+  dispatch({
+    type: 'FETCHING_ME',
+    promise: fetchAPI('/me')
+  })
 }
 
 export function fetchUnits(dispatch) {
-  dispatch({ type: 'FETCHING_UNITS' })
-
-  return fetch(api('/units'), fetchOpts)
-    .then(response => response.json())
-    .then(json => {
-      dispatch({
-        type: 'FETCHING_UNITS_SUCCESS',
-        data: json.data,
-        receivedAt: Date.now()
-      })
-    })
-    .catch(e => {
-      dispatch({
-        type: 'FETCHING_UNITS_ERROR',
-        message: e,
-        receivedAt: Date.now()
-      })
-    })
+  dispatch({
+    type: 'FETCHING_UNITS',
+    promise: fetchAPI('/units')
+  })
 }
 
 export function markComplete(dispatch, uuid) {
-  dispatch({
-    type: 'FETCHING_MARK_COMPLETE',
-    uuid: uuid
-  })
-
   const body = new Blob([JSON.stringify({
     data: {
       type: 'unit',
@@ -79,24 +42,9 @@ export function markComplete(dispatch, uuid) {
     }
   })], { type: 'application/json' })
 
-  return fetch(api('/units/' + uuid), { ...fetchOpts, body: body, method: 'PATCH' })
-    .then(response => response.json())
-    .then(json => {
-      if (json.hasOwnProperty('errors')) {
-        return Promise.reject(json.errors[0].title)
-      }
-
-      dispatch({
-        type: 'FETCHING_MARK_COMPLETE_SUCCESS',
-        data: json.data,
-        receivedAt: Date.now()
-      })
-    })
-    .catch(e => {
-      dispatch({
-        type: 'FETCHING_MARK_COMPLETE_ERROR',
-        message: e,
-        receivedAt: Date.now()
-      })
-    })
+  dispatch({
+    type: 'FETCHING_MARK_COMPLETE',
+    uuid: uuid,
+    promise: fetchAPI('/units/' + uuid, { body: body, method: 'PATCH' })
+  })
 }
---
+++ app/middleware/index.jsx
@@ -0,0 +1,52 @@
+/*
+ * This middleware makes some assumptions about the actions it ends up
+ * procesing:
+ *
+ *   - A Promise object is given as a `promise` property of the action
+ *   - This Promise came from the Fetch API (e.g. a `fetch()` call)
+ *   - We're fetching something from a JSON API that follows spec
+ */
+const fetchJSONMiddleware = ({ dispatch }) => (next) => (action) => {
+  // No Promise? </3 Move along…
+  if (!action.promise) {
+    return next(action)
+  }
+
+  // We still want to give the next middlware's dispatcher this action
+  next(action)
+
+  const { type, promise, ...args } = action
+
+  return promise
+    .then(res => ({ res: res, payload: res.json() }))
+    .then(({ res, payload }) => {
+      if (res.status >= 200 && res.status < 300) {
+        return payload
+      }
+
+      // Prefer error messages following the JSON API spec.
+      if (payload.hasOwnProperty('errors')) {
+        return Promise.reject(payload.errors[0].title)
+      } else {
+        return Promise.reject(res.statusText)
+      }
+    })
+    .then(payload => {
+      dispatch({
+        type: type + '_SUCCESS',
+        data: payload.data,
+        receivedAt: Date.now(),
+        ...args
+      })
+    })
+    .catch(e => {
+      dispatch({
+        type: type + '_ERROR',
+        message: e,
+        receivedAt: Date.now(),
+        ...args
+      })
+    })
+}
+
+export { fetchJSONMiddleware }
